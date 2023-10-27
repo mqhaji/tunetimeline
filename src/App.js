e010b6c3213cfbdcfea64cc260517794110394e5
Musa Haji <mqhaji@vt.edu>
2023-10-27 01:51:03 -0400

    added home and login folders

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+
 function App() {
   return (
     <div className="App">
-       TuneTimeline
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/home" component={Home} />
+        </Switch>
+      </Router>
     </div>
   );
 }
